e04b051b676db86c8a5c6bfc1aea88472a24e41c
Erik Malmstrom-Partridge <erikdevelopments@gmail.com>
2015-01-09 09:20:21 -0500

    Config now checks if connected to internet

--- main/java/com/takeoffsim/main/Config.java
+++ main/java/com/takeoffsim/main/Config.java
@@ -9,8 +9,16 @@
 package com.takeoffsim.main;
 
 
+import com.jcabi.aspects.Cacheable;
 import com.takeoffsim.services.Serialize;
+import lombok.extern.apachecommons.CommonsLog;
 
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.util.concurrent.TimeUnit;
+
+@CommonsLog
 public class Config {
 
     public static String nameOfSim;
@@ -18,5 +26,16 @@ public class Config {
 
     public static String getPath() {
         return Serialize.homeDirectory();
+    }
+    
+    @Cacheable(lifetime = 70, unit = TimeUnit.SECONDS, forever = false)
+    public static boolean webConnected(){
+        try{
+            InetAddress ip = Inet4Address.getByName("takeoffsim.com");
+            return ip.isReachable(450);
+        }catch(IOException e){
+            log.info("Cannot connect to TakeoffSim.com");
+            return false;
+        }
     }
 }
